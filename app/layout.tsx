7cf0ae5ca640076a7e30ab7061589dededc898bd
Prajan Gowtham <prajangowtham2004@gmail.com>
2025-12-16 18:45:10 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/tvk-flag.jpeg" type="image/jpeg" />
+        <link rel="icon" href="/nvk-flag.jpeg" type="image/jpeg" />
       </head>
       <body
         className={`font-sans ${GeistSans.variable} ${GeistMono.variable} antialiased`}
