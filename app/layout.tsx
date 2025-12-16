255d9f959147550703660dfe53e3db13d72bbb41
Prajan Gowtham <prajangowtham2004@gmail.com>
2025-12-16 19:02:29 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,17 @@ import { GeistSans } from "geist/font/sans"
 import type { Metadata } from "next"
 import type React from "react"
 import { Suspense } from "react"
-import { Toaster } from "sonner"; // 👈 use sonner instead of react-hot-toast
+import { Toaster } from "sonner"
 import "./globals.css"
 
 export const metadata: Metadata = {
   title: "Prajan Gowtham - Portfolio",
-  description: "B.Tech Computer Science Engineering | AI/ML Enthusiast | Full Stack Developer",
+  description:
+    "B.Tech Computer Science Engineering | AI/ML Enthusiast | Full Stack Developer",
   generator: "v0.app",
+  verification: {
+    google: "YixObLykn7tYDA27NPk9ybj3rNWCevcuZG5kqleoH58",
+  },
 }
 
 export default function RootLayout({
@@ -26,7 +30,7 @@ export default function RootLayout({
       <body
         className={`font-sans ${GeistSans.variable} ${GeistMono.variable} antialiased`}
       >
-        <Toaster position="top-right" richColors /> {/* ✅ Sonner toaster */}
+        <Toaster position="top-right" richColors />
         <Suspense fallback={null}>{children}</Suspense>
         <Analytics />
       </body>
